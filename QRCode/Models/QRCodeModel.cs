7916e67d4664ac9bae36a2d1a5c688b304ab8735
Trương Công Thái <thaitc@sphinxjsc.com>
2022-11-09 09:45:53 +0700

    Add project files.

---
+++ QRCode/Models/QRCodeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace QRCode.Models
+{
+    public class QRCodeModel
+    {
+        [Display(Name = "Enter QRCode Text")]
+        public string QRCodeText { get; set; }
+    }
+}

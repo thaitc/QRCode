81e7d7bb92cd86f6be0cc1d977df2a2c46b0de16
Trương Công Thái <thaitc@sphinxjsc.com>
2022-11-09 10:25:12 +0700

    update

---
+++ QR/Controllers/QrCodeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QR.Models.Dto;
+using QR.Models;
+using QR.Service.Interface;
+
+namespace QR.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        private readonly IQrCodeService _qrCodeInterface;
+
+        public QrCodeController(IQrCodeService qrCodeInterface)
+        {
+            _qrCodeInterface = qrCodeInterface;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateQRCode()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateQRCode(QRCodeModel qRCode)
+        {
+            var genQr = new GenQrDto();
+            genQr.accountNo = qRCode.AccountNumber;
+            genQr.accountName = qRCode.AccountName;
+            genQr.acqId = "970423";
+            genQr.addInfo = "Xin chao VN";
+            genQr.amount = qRCode.TransferAmount.ToString();
+            genQr.template = "compact";
+            var genVietQr = _qrCodeInterface.GenQrCode(genQr);
+            if (genVietQr == null)
+            {
+                return View();
+            }
+            string base64 = genVietQr.data.qrDataURL;
+            ViewBag.QrCodeUri = base64;
+            return View();
+        }
+    }
+}
---
+++ QR/Models/Dto/GenQrDto.cs
@@ -0,0 +1,13 @@
+﻿namespace QR.Models.Dto
+{
+    public class GenQrDto
+    {
+        public string accountNo { get; set; }
+        public string accountName { get; set; }
+        public string acqId { get; set; }
+        public string addInfo { get; set; }
+        public string amount { get; set; }
+        public string template { get; set; }
+
+    }
+}
